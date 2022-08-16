9d824c3696e3a5e41e963b3d71b825f0c2ed1138
BangTran <bangtranluong195@gmail.com>
2022-08-16 16:27:47 +0200

    update Faster RCNN config

---
+++ train.py
@@ -0,0 +1,28 @@
+from mmcv import Config
+import mmcv
+from mmdet.datasets import build_dataset
+from mmdet.models import build_detector
+from mmdet.apis import train_detector
+import os.path as osp
+
+def run():
+    cfg = Config.fromfile('./model/3_classes/faster_rcnn_x101_64x4d_fpn_1x_coco.py')
+
+    # Build dataset
+    datasets = [build_dataset(cfg.data.train)]
+
+    # Build the detector
+    model = build_detector(cfg.model)
+    # Add an attribute for visualization convenience
+    model.CLASSES = datasets[0].CLASSES
+
+
+    # Create work_dir
+    mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+    cfg.dump(osp.join(cfg.work_dir, 'faster_rcnn_x101_64x4d_fpn_1x_coco.py'))
+
+    train_detector(model, datasets, cfg, distributed=False, validate=True)
+
+
+if __name__ == '__main__':
+    run()
