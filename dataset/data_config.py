88f41ecb56a48ef61be7a134fcd4eb798c503e51
BangTran <bangtranluong195@gmail.com>
2022-09-20 15:33:28 +0200

    update 640

--- dataset/data_config.py
+++ dataset/data_config.py
@@ -23,7 +23,13 @@ data_configs = {
             'train_image_path': '/content/drive/MyDrive/data_3_classes/size_224/images/train_images',
             'val_annotation_file': '/content/drive/MyDrive/data_3_classes/size_224/annotations/val_annotations_coco.json',
             'val_image_path': '/content/drive/MyDrive/data_3_classes/size_224/images/val_images',
-
+        },
+        '640': {
+            'data_root': '/content/drive/MyDrive/data_3_classes/size_640/images',
+            'train_annotation_file': '/content/drive/MyDrive/data_3_classes/size_640/annotations/train_annotations_coco.json',
+            'train_image_path': '/content/drive/MyDrive/data_3_classes/size_640/images/train_images',
+            'val_annotation_file': '/content/drive/MyDrive/data_3_classes/size_640/annotations/val_annotations_coco.json',
+            'val_image_path': '/content/drive/MyDrive/data_3_classes/size_640/images/val_images',
         }
     }
 }
